4c3a3bf6154b7c1caa556b64ad4c9b566046434e
Jeremy Jacob <github@jeremyjacob.dev>
2022-04-25 15:54:27 -0700

    Make Bearer optional

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -1,10 +1,13 @@
 import type { Request } from 'express';
 import { getSession } from './db';
 
-const API_KEY = 'Bearer ' + process.env.FORAGER_API_KEY;
+const API_KEY = process.env.FORAGER_API_KEY;
 
 export async function authCheck(req: Request) {
 	const session = req.cookies?.session_id;
 	const hasSession = await getSession(session);
-	return hasSession || req.headers.authorization == API_KEY;
+	const apiKey =
+		req.headers?.authorization == API_KEY ||
+		req.headers?.authorization == 'Bearer ' + API_KEY;
+	return hasSession || apiKey;
 }
---
+++ frontend/src/lib/loader.ts
@@ -0,0 +1,42 @@
+export enum Endpoint {
+	Dashboard = 'dashboard',
+	Count = 'count',
+	Results = 'results',
+	Tags = 'tags',
+	MachineControl = 'machineControl',
+	Login = 'login',
+	Register = 'register'
+}
+
+const endpoints = {
+	[Endpoint.Dashboard]: { method: 'GET' },
+	[Endpoint.Count]: { method: 'GET' },
+	[Endpoint.Results]: { method: 'GET' },
+	[Endpoint.Tags]: { method: 'GET' },
+	[Endpoint.MachineControl]: { method: 'POST' },
+	[Endpoint.Login]: { method: 'POST' },
+	[Endpoint.Register]: { method: 'POST' }
+} as { [endpoint: string]: { method: string } };
+
+const apiURL = 'http://localhost:8000/api/';
+
+export async function load(
+	endpoint: Endpoint,
+	{ query = '', body = {} }: { query?: string; body?: any } = {}
+) {
+	const { method } = endpoints[endpoint];
+	const opts: RequestInit = {
+		method,
+		credentials: 'include'
+	};
+	if (body && method == 'POST') {
+		opts.body = JSON.stringify(body);
+		if (!opts.headers) opts.headers = {};
+		opts.headers['Content-Type'] = 'application/json';
+	}
+
+	const res = await fetch(`${apiURL}${endpoint}?${query}`, opts);
+	const json = await res.json();
+	if (json?.mustLogin) location.pathname = 'login';
+	return json;
+}
