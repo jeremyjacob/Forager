2c3fc7fd9bcbdc431789219bee1ad05f449226eb
Jeremy Jacob <github@jeremyjacob.dev>
2022-04-25 18:44:12 -0700

    test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import type { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
-
+//
 export const app = express();
 
 const corsOrigins = ['https://forager.jeremyjacob.dev'];
