c42c0779fd043bdb6bba5240c1a52732a4470880
Jeremy Jacob <github@jeremyjacob.dev>
2022-04-25 18:56:18 -0700

    test

--- backend/src/routes/github.ts
+++ backend/src/routes/github.ts
@@ -24,12 +24,12 @@ app.post('/api/github', async (req, res) => {
 				.createHmac('sha256', API_KEY)
 				.update(chunk.toString())
 				.digest('hex');
-		if (headerSig == sig) {
+		console.log(sig);
+		if (headerSig === sig) {
 			// request is from GitHub
 			pullRepo();
 			console.log('Got pull notification from GitHub...');
 			res.send({ ok: 1 });
-		}
+		} else res.status(401).send({ ok: 0 });
 	});
-	setTimeout(() => res.status(401).send({ ok: 0 }), 2000);
 });
