e77abbb39d0da3daca7bb8f5082c827bea833940
Jeremy Jacob <github@jeremyjacob.dev>
2022-04-25 18:33:21 -0700

    Fix nodemon for prod

---
+++ backend/src/routes/github.ts
@@ -0,0 +1,26 @@
+import { apiKeyCheck, API_KEY } from 'src/auth';
+import { app } from '../main';
+import crypto from 'crypto';
+import { exec } from 'child_process';
+
+function pullRepo() {
+	exec(
+		['cd ~/Forager/backend', 'git reset --hard HEAD', 'git pull'].join('&&')
+	);
+}
+
+app.post('/api/github', async (req, res) => {
+	const headerSig = req.header['X-Hub-Signature-256'];
+	req.on('data', (chunk) => {
+		let sig =
+			'sha256=' +
+			crypto
+				.createHmac('sha256', API_KEY)
+				.update(chunk.toString())
+				.digest('hex');
+		if (headerSig == sig) {
+			// request is from GitHub
+			pullRepo();
+		}
+	});
+});
