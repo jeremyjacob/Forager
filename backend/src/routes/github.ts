046c5ce102748723ba5105bc89d38a41d7656bca
Jeremy Jacob <github@jeremyjacob.dev>
2022-04-30 19:40:14 -0700

    Try new github pull method

--- backend/src/routes/github.ts
+++ backend/src/routes/github.ts
@@ -4,24 +4,27 @@ import crypto from 'crypto';
 import { exec, spawn } from 'child_process';
 
 function pullRepo() {
+	console.log('Refreshing local files...');
 	const executor = exec(
-		// [
-		// 	[
-		// 		'git fetch',
-		// 		'git reset --hard HEAD',
-		// 		'git clean -f -d',
-		// 		`git merge -m '@{u}'`,
-		// 	],
-		// 	['cd ~/Forager/frontend', 'pnpm install', 'pnpm build'],
-		// 	['cd ~/Forager/backend', 'pnpm install'],
-		// ]
-		// 	.map((l) => l.join('&&'))
-		// 	.join(';'),
-		'touch test'
+		[
+			[
+				'git fetch',
+				'git reset --hard HEAD',
+				'git clean -f -d',
+				`git merge -m '@{u}'`,
+			],
+			['cd ~/Forager/frontend', 'pnpm install', 'pnpm build'],
+			['cd ~/Forager/backend', 'pnpm install'],
+		]
+			.map((l) => l.join('&&'))
+			.join(';')
 	);
 	executor.stdout.pipe(process.stdout);
 	executor.stderr.pipe(process.stderr);
-	executor.on('exit', process.exit(0));
+	executor.on('exit', () => {
+		console.log('Done!');
+		process.exit(0);
+	});
 }
 
 app.post('/github', async (req, res) => {
@@ -36,7 +39,6 @@ app.post('/github', async (req, res) => {
 		// request is from GitHub
 		// setTimeout(pullRepo, 2000);
 		pullRepo();
-		console.log('Got pull notification from GitHub...');
 		res.send({ ok: 1 });
 	} else res.status(401).send({ ok: 0 });
 });
