40978c94420a877be421dd7307bceec64da0e37e
Jeremy Jacob <github@jeremyjacob.dev>
2022-05-11 20:22:55 -0700

    Remove pagintation from scrape endpoint

--- backend/src/routes/scrape.ts
+++ backend/src/routes/scrape.ts
@@ -27,15 +27,13 @@ app.get('/scrape', async (req, res) => {
 		arrayFormat: 'comma',
 	});
 	const limit = parseInt(unArray(query.limit) || '100');
-	const lastPage = unArray(query.lastPage);
-	const filter = await makeFilter(query, {client: false});
+	const filter = await makeFilter(query, { client: false });
 	const date = new Date();
 	filter.fetches = { $not: { $gte: FETCHES_TARGET } };
 	filter.lock = { $not: { $gt: date } };
 
 	const data = await await getDomains(filter, {
 		limit,
-		lastPage: unArray(lastPage),
 		lock: true,
 	});
 	let body = data.map(({ domain, _id }) => ({ domain, _id }));
