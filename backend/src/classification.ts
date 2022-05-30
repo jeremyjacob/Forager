fcbaff9cfb522148cead4b20907f62303c5cfbb5
Jeremy Jacob <github@jeremyjacob.dev>
2022-05-30 13:59:06 -0700

    Comment out classification

--- backend/src/classification.ts
+++ backend/src/classification.ts
@@ -1,33 +1,33 @@
 // spawn child python process to run the model
 
-import { type ChildProcessWithoutNullStreams, spawn } from 'child_process';
+// import { type ChildProcessWithoutNullStreams, spawn } from 'child_process';
 
-let child: ChildProcessWithoutNullStreams;
-spawnClassifier();
+// let child: ChildProcessWithoutNullStreams;
+// spawnClassifier();
 
-function spawnClassifier() {
-	child = spawn('python3', ['forager_finetune_distilbert.py'], {
-		cwd: './classification',
-	});
+// function spawnClassifier() {
+// 	child = spawn('python3', ['forager_finetune_distilbert.py'], {
+// 		cwd: './classification',
+// 	});
 
-	child.stderr.on('data', (e) =>
-		console.error('Python model error', e.toString())
-	);
+// 	child.stderr.on('data', (e) =>
+// 		console.error('Python model error', e.toString())
+// 	);
 
-	child.on('exit', () => {
-		console.log('Restarting python model...');
-		spawnClassifier();
-	});
-}
+// 	child.on('exit', () => {
+// 		console.log('Restarting python model...');
+// 		spawnClassifier();
+// 	});
+// }
 
-export async function modelInferenceArray(data: string[]): Promise<number[]> {
-	return new Promise((resolve) => {
-		child.stdout.on('data', (data) => {
-			child.stdout.removeAllListeners('data');
-			const results: number[] = JSON.parse(data.toString());
-			return resolve(results);
-		});
+// export async function modelInferenceArray(data: string[]): Promise<number[]> {
+// 	return new Promise((resolve) => {
+// 		child.stdout.on('data', (data) => {
+// 			child.stdout.removeAllListeners('data');
+// 			const results: number[] = JSON.parse(data.toString());
+// 			return resolve(results);
+// 		});
 
-		child.stdin.write(JSON.stringify(data) + '\n');
-	});
-}
+// 		child.stdin.write(JSON.stringify(data) + '\n');
+// 	});
+// }
